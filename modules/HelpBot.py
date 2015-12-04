ed68262f793b2f74b7b839f961a2083adbc9a8ac
Prince S. Valluri <psvallur@psvallur-mn1.linkedin.biz>
2015-12-04 10:44:17 +0530

    Help Message fix

--- modules/HelpBot.py
+++ modules/HelpBot.py
@@ -17,9 +17,12 @@ In order to provide support, kindly send\n\
 \n\
 Send your Location to get instant help from someone near you. We will try to connect that person to you.\n\
 \n\
+You can tell us how to improve by sending feedback and if you like the service\n\
+#feedback <message>\n\
+#like\n\
+\n\
 PS: Location is optional\n\n\
-PPS: This number can be reached only via WhatsApp.\n\
-"
+PPS: This number can be reached only via WhatsApp."
 
 class HelpBot():
   def __init__(self):
